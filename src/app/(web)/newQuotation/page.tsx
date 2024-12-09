3a7ddad916198b2a91b448bc761a0358189421d1
unknown <juanmavargasr@gmail.com>
2024-12-09 14:52:27 -0500

    fix(newProduct): suspense problem solved

--- src/app/(web)/newQuotation/page.tsx
+++ src/app/(web)/newQuotation/page.tsx
@@ -1,19 +1,19 @@
 // import { Button } from "@/components/ui/button";
 "use client";
 
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 
-import styles from "./newQuiotation.module.scss";
+// import styles from "./newQuiotation.module.scss";
 import ProductQuotation from "./components/productQuotation/productQuotation";
 
 export default function Page() {
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
 
-  const quotationId = searchParams.get("id");
+  // const quotationId = searchParams.get("id");
   return (
     <div className="flex flex-col w-full justify-center items-center">
       <div className="w-[800px] justify-center items-center">
-        <p className={styles.title}>Quote #{quotationId}</p>
+        {/* <p className={styles.title}>Quote #{quotationId}</p> */}
 
         <ProductQuotation />
       </div>
